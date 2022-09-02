6a3f2d5a7a1ed588d3fb1cb53308981726e01380
mishana <websoullabsmishana@gmail.com>
2022-09-02 14:56:46 +0530

    edited items

--- src/pages/Whoweare/Whoweare.js
+++ src/pages/Whoweare/Whoweare.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Section1 from './Section1';
 import Section2 from './Section2';
@@ -23,5 +22,3 @@ function Whoweare() {
 }
 
 export default Whoweare
-=======
->>>>>>> 2a93fcf31e737620dd89d0fef3faa8b5fd056949
