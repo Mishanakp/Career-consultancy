eb289099fdfcffb104f8b99746940d7a1f861a72
ShahidaJubil <shahidajubil@gmail.com>
2022-09-02 11:45:35 +0530

    book

--- src/navigation/Navigate.js
+++ src/navigation/Navigate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-//import Footer from "../components/footer/Footer";
-//import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 import Home from '../pages/home/Home'
 import Contact from '../pages/contactus/Contact';
 import Payment from '../Authentication/Payment/Payment';
@@ -36,7 +36,7 @@ export default function Navigate() {
     return (
         <Router>
             <div>
-              {/* <div><Header/></div>*/ }
+              <div><Header/></div>
                 <Routes>
                     <Route exact path="/" element={<Home />}></Route>
                     
@@ -85,7 +85,7 @@ export default function Navigate() {
 
               
                 </Routes>
-               {/* <div><Footer/></div>*/ }
+                <div><Footer/></div>
             </div>
         </Router>
 
