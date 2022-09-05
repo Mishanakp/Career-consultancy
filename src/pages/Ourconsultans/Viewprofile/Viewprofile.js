fd12e6d3b142c249bd505078c34a553c3470d319
ShahidaJubil <shahidajubil@gmail.com>
2022-09-05 16:29:07 +0530

    edited

--- src/pages/Ourconsultans/Viewprofile/Viewprofile.js
+++ src/pages/Ourconsultans/Viewprofile/Viewprofile.js
@@ -9,6 +9,7 @@ import classes from './Viewprofile.module.css';
 import education from '../../../Assets/booksession/education.png';
 import tick from '../../../Assets/booksession/tick.png';
 import language from '../../../Assets/booksession/language.png';
+import Booksession from '../Booksession/Booksession';
 
 
 
@@ -24,7 +25,7 @@ function Viewprofile() {
             <div className={classes.top}>
             </div>
           </Grid>
-          <Grid item  lg={3.4} md={3} sm={12} xs={12}>
+          <Grid item  xl={3.4} lg={3.4} md={3} sm={12} xs={12}>
           <img className={classes.profileimgn} src='https://www.felicity.care/assets/images/homepage-slides/zoha-slide.webp' alt='counselor1'/>
           
             <br/>
@@ -35,7 +36,7 @@ function Viewprofile() {
           </Grid>
 
 
-          <Grid item xl={6} lg={8.5} md={9} sm={12} xs={12}>
+          <Grid item xl={8} lg={8.5} md={9} sm={12} xs={12}>
   <div className={classes.viewsha}>
     <ul>
       <li>
@@ -56,8 +57,8 @@ function Viewprofile() {
 
 </Grid>
          
-
-          <Grid item xl={4} lg={7.5} md={7.7} sm={10.7} xs={10.3} className={classes.aboutcounsellor}>
+<Grid item xl={3.7} lg={3.9} md={3.6} sm={0.7} xs={0.85} ></Grid>
+          <Grid item xl={7} lg={7.5} md={7.7} sm={10.8} xs={10.3} className={classes.aboutcounsellor}>
           <h3 style={{color:'#0e314c'}}><b>About</b></h3>
                 <p >
               Have been conducting career guidance workshops in 30 schools in Mumbai, Pune, 
@@ -67,11 +68,11 @@ function Viewprofile() {
                  career queries on email over the last 5 years Commerce Graduate and an MBA in Finance from IIM Indore.</p>
                  </Grid>
 
-                 <Grid item lg={3} md={3.2} sm={3.5} xs={11} className={classes.counsellorbutton}>
+                 <Grid item lg={3} md={3.2} sm={3.1} xs={11} className={classes.counsellorbutton}>
+                 <Link to="/Booksession">
               <button className={classes.probutton}>
-               
-                <Link to="/booksession" > <span style={{color:'white'}}>Book Session</span></Link>
-                </button>
+                <span style={{color:'white'}}>Book Session</span>
+                </button></Link>
                 </Grid>
             
                
